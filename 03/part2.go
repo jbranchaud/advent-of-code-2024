909e691c6e1081b6639efea4b43dcb74cef9f0a1
jbranchaud <jbranchaud@gmail.com>
2024-12-03 14:19:17 -0600

    Pull out separate function for part2

--- 03/part2.go
+++ 03/part2.go
@@ -16,13 +16,9 @@ func part2(debug bool) {
 		return
 	}
 
-	mulPattern := `mul\((\d{1,3}),(\d{1,3})\)`
-	mulMatcher := regexp.MustCompile(mulPattern)
-
-	doPattern := `do\(\)`
-	doMatcher := regexp.MustCompile(doPattern)
-	dontPattern := `don't\(\)`
-	dontMatcher := regexp.MustCompile(dontPattern)
+	mulMatcher := compileMatcher(`mul\((\d{1,3}),(\d{1,3})\)`)
+	doMatcher := compileMatcher(`do\(\)`)
+	dontMatcher := compileMatcher(`don't\(\)`)
 
 	var sum int
 
@@ -41,49 +37,6 @@ func part2(debug bool) {
 		// cursors for tracking place in each _Indexes slice
 		mulCursor, doCursor, dontCursor := 0, 0, 0
 
-		parseMultiplicationMatch := func(program string, mulIndexes [][]int, mulCursor *int, status string, debug bool) int {
-			match := mulIndexes[*mulCursor]
-			*mulCursor++
-			if status == "DONT" {
-				if debug {
-					fmt.Println("x", program[match[0]:match[1]])
-				}
-
-				return 0
-			}
-
-			op1, err := strconv.Atoi(program[match[2]:match[3]])
-			if err != nil {
-				panic(fmt.Errorf("error parsing op1 int: %v", err))
-			}
-			op2, err := strconv.Atoi(program[match[4]:match[5]])
-			if err != nil {
-				panic(fmt.Errorf("error parsing op2 int: %v", err))
-			}
-
-			mulResult := op1 * op2
-			if debug {
-				fmt.Println(program[match[0]:match[1]], "=>", mulResult)
-			}
-			return mulResult
-		}
-
-		parseDoMatch := func(doCursor *int, status *string, debug bool) {
-			*doCursor++
-			*status = "DO"
-			if debug {
-				fmt.Println("- DO() -")
-			}
-		}
-
-		parseDontMatch := func(dontCursor *int, status *string, debug bool) {
-			*dontCursor++
-			*status = "DONT"
-			if debug {
-				fmt.Println("- DON'T() -")
-			}
-		}
-
 		// step through ever single position in the `program` string
 		// if the position matches a recognized syntax index, then parse it
 		// otherwise no-op and proceed
@@ -100,4 +53,51 @@ func part2(debug bool) {
 	}
 
 	fmt.Println("Total: ", sum)
+}
+
+func compileMatcher(pattern string) *regexp.Regexp {
+	return regexp.MustCompile(pattern)
+}
+
+func parseMultiplicationMatch(program string, mulIndexes [][]int, mulCursor *int, status string, debug bool) int {
+	match := mulIndexes[*mulCursor]
+	*mulCursor++
+	if status == "DONT" {
+		if debug {
+			fmt.Println("x", program[match[0]:match[1]])
+		}
+
+		return 0
+	}
+
+	op1, err := strconv.Atoi(program[match[2]:match[3]])
+	if err != nil {
+		panic(fmt.Errorf("error parsing op1 int: %v", err))
+	}
+	op2, err := strconv.Atoi(program[match[4]:match[5]])
+	if err != nil {
+		panic(fmt.Errorf("error parsing op2 int: %v", err))
+	}
+
+	mulResult := op1 * op2
+	if debug {
+		fmt.Println(program[match[0]:match[1]], "=>", mulResult)
+	}
+	return mulResult
+}
+
+func parseDoMatch(doCursor *int, status *string, debug bool) {
+	*doCursor++
+	*status = "DO"
+	if debug {
+		fmt.Println("- DO() -")
+	}
+}
+
+func parseDontMatch(dontCursor *int, status *string, debug bool) {
+	*dontCursor++
+	*status = "DONT"
+	if debug {
+		fmt.Println("- DON'T() -")
+	}
 }
