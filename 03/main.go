5cc994b61f37f36ec33324e10f4797613e174a55
jbranchaud <jbranchaud@gmail.com>
2024-12-03 14:21:53 -0600

    Use flag package to parse --debug flag

--- 03/main.go
+++ 03/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	var debug bool
+	flag.BoolVar(&debug, "debug", false, "turns on debug mode, extra logging")
+	flag.Parse()
+
+	positionalArgs := flag.Args()
+
+	if len(positionalArgs) < 1 {
 		fmt.Println("Please specify which part to run: 1 or 2")
 		os.Exit(1)
 	}
 
-	debug := len(os.Args) > 2 && os.Args[2] == "--debug"
-
-	switch os.Args[1] {
+	switch positionalArgs[0] {
 	case "1":
 		part1()
 	case "2":
 		part2(debug)
 	default:
-		fmt.Printf("Invalid part specified: %s. Please use 1 or 2\n", os.Args[1])
+		fmt.Printf("Invalid part specified: %s. Please use 1 or 2\n", positionalArgs[0])
 		os.Exit(1)
 	}
 }
